ae41c64cfecd1bdb5e7b740b5ce5210d4eaa9af1
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-26 15:17:39 -0700

    Set up project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="bg-blue-500 w-full h-[100vh] text-white p-4 text-center">
-      Hello Tailwind in React 🚀
+    <div className="bg-blue-500 text-voilet-300 w-full h-[100vh] p-4 text-center">
+      Hello Tailwind in React
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}", // React ke sabhi component files ke liye
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sanf-serif"],
+        general: ["general", "sanf-serif"],
+        "circular-web": ["circular-web", "sanf-serif"],
+        "robert-medium": ["robert-medium", "sanf-serif"],
+        "robert-regular": ["robert-regular", "sanf-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        voilet: {
+          300: "#5724FF",
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
