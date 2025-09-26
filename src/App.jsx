ac5971c8a6efdf3eeb9235d00e8f04136c0118e6
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-26 14:28:28 -0700

    Initailize again correctly

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 w-full h-[100vh] text-white p-4 text-center">
+      Hello Tailwind in React 🚀
+    </div>
+  );
+};
+
+export default App;
