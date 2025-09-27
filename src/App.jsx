68d6db1299085f6284dbed475864ee9d664a43dd
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-26 18:30:01 -0700

    Working on About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Hero from "./component/Hero";
+import About from "./component/About";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="h-screen bg-black w-full"></section>
+      <About />
     </main>
   );
 };
