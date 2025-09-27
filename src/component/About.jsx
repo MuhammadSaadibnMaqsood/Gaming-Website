64adc005e6df7bc96c277d0740851d835b02283e
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-27 05:16:17 -0700

    Complete About section

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import React from "react";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 const About = () => {
@@ -30,9 +31,10 @@ const About = () => {
           Welcome to Zentry
         </h2>
 
-        <div className="mt-5 text-center text-5xl uppercase leading-[0.8] md:text-[4rem]">
-          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle
+          title=" Disc<b>o</b>ver the world's <br  l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
@@ -41,13 +43,13 @@ const About = () => {
       </div>
 
       <div className="h-dvh w-screen " id="clip">
-        <div className="mask-clip-path about-image">
+        <d iv className="mask-clip-path about-image">
           <img
             src="img/about.webp"
             alt="background"
             className="absolute left-0 top-0 size-full object-cover"
           />
-        </div>
+        </d>
       </div>
     </div>
   );
