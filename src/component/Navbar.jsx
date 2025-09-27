1522e10573f6332c3c38033332ffd6e73b2f3c60
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-27 05:28:38 -0700

    Still working on nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useRef } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Navbar = () => {
   const navContainerRef = useRef(null);
@@ -7,9 +9,22 @@ const Navbar = () => {
       ref={navContainerRef}
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
-        <header className="absolute top-1/2 w-full">
-
-        </header>
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex p-4 size-full items-center justify-between">
+          {/* LEFT SIDE  */}
+          <div className="flex items-center gap-7">
+            <img className="w-10" src="/img/logo.png" alt="logo" />
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-ceneter gap-1"
+            />
+          </div>
+          {/* RIGHT SIDE  */}
+          <div></div>
+        </nav>
+      </header>
     </div>
   );
 };
